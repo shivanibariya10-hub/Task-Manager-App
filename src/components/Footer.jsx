8d68fe6430dea9409244d742525ae997e22be319
shivanibariya10-hub <shivanibariya10@gmail.com>
2026-03-20 12:00:15 +0530

    first commit

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ textAlign: "center", padding: "20px" }}>
-      <p>© 2026 Task Manager App</p>
-    </footer>
-  );
-}
-
-export default Footer;
